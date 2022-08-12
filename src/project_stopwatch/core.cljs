3a9eb27f83f18050107c1d93476e2ebe2116b0e0
Tim <77175275+DerTev@users.noreply.github.com>
2022-08-12 21:31:56 +0200

    Add Auto-Save

--- src/project_stopwatch/core.cljs
+++ src/project_stopwatch/core.cljs
@@ -28,11 +28,11 @@
 ; Init app
 (defn mount-root []
   (d/render [:div [:div {:class ["header"]}
-              [:h1 "Project Stopwatch"]
-              (if (boolean (.-chrome js/window)) [:div {:id "actions"} 
-                                                  [:i {:onClick #(load) :class "fa-solid fa-folder-open" :style {:cursor "pointer"}}]
-                                                  [:i {:onClick #(backup) :class "fa-solid fa-folder-tree ms-2" :style {:cursor "pointer"}}]
-                                                  [:i {:onClick #(save) :class "fa-solid fa-floppy-disk ms-2" :style {:cursor "pointer"}}]])]
+                   [:h1 "Project Stopwatch"]
+                   (if (boolean (.-chrome js/window)) [:div {:id "actions"}
+                                                       [:i {:onClick #(load) :class "fa-solid fa-folder-open" :style {:cursor "pointer"}}]
+                                                       [:i {:onClick #(backup) :class "fa-solid fa-folder-tree ms-2" :style {:cursor "pointer"}}]
+                                                       [:i {:onClick #(save) :class "fa-solid fa-floppy-disk ms-2" :style {:cursor "pointer"}}]])]
              [:br]
              [:div {:id ["main"]}
               (if (boolean (.-chrome js/window))
@@ -45,13 +45,16 @@
                    [:th {:scope "col"} "Honorar"]
                    [:th {:scope "col"} "Aktionen"]]]
                  [:tbody
-                   (for [session @data]
-                     [:tr [:td (get session :project)]
-                      [:td (get session :date)]
-                      [:td (get session :time)]
-                      [:td (get session :fee)]
-                      [:td [:i {:onClick #(js/alert "Coming soon") :class "fa-solid fa-trash" :style {:cursor "pointer"}}]]])]]
-                [:p "Bitte öffnen Sie diese Seite mit einem " [:a {:href "https://en.wikipedia.org/wiki/Chromium_(web_browser)#Active" :target :blank} "chromium basierenden Browser"] "!"])]] (.getElementById js/document "app")))
+                  (for [session @data]
+                    [:tr [:td (get session :project)]
+                     [:td (get session :date)]
+                     [:td (get session :time)]
+                     [:td (get session :fee)]
+                     [:td [:i {:onClick #(js/alert "Coming soon") :class "fa-solid fa-trash" :style {:cursor "pointer"}}]]])]]
+                [:p "Bitte öffnen Sie diese Seite mit einem " [:a {:href "https://en.wikipedia.org/wiki/Chromium_(web_browser)#Active" :target :blank} "chromium basierenden Browser"] "!"])]] (.getElementById js/document "app"))
+                
+                ; Enable Auto-Save
+                (add-watch data :auto-save save))
 
 (defn ^:export init! []
   (mount-root))
